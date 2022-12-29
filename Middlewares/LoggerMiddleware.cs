1ed6bb8170c4b460d6cca6d20d7224985921a88f
mengolo.olive@gmail.com <mengolo.olive@gmail.com>
2022-12-29 21:55:01 +0100

    added middleware

---
+++ Middlewares/LoggerMiddleware.cs
@@ -0,0 +1,37 @@
+﻿using Logger.Tools.Interfaces;
+using Microsoft.AspNetCore.Builder;
+using Microsoft.AspNetCore.Http;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net.Http;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Logger.Middlewares
+{
+    public class LoggerMiddleware
+    {
+        private readonly RequestDelegate _next;
+        public LoggerMiddleware(RequestDelegate next)
+        {
+            _next = next;
+        }
+
+        public async Task Invoke(HttpContext context)
+        {
+            HttpRequest request = context.Request;
+            IDisplay._request = request;
+
+            await _next(context);
+        }
+    }
+
+    public static class ALoggerMiddlewareExtensions
+    {
+        public static IApplicationBuilder UseALoggerMiddleware(this IApplicationBuilder app)
+        {
+            return app.UseMiddleware<LoggerMiddleware>();
+        }
+    }
+}
