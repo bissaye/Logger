a0aa12cd39add63e1ca8b4e56440f421f3698c41
bissaye <bissaye.com@gmail.com>
2023-01-10 10:47:58 +0100

    création des classe d'Exception spécifiques à Logger

---
+++ Exceptions/LoggerException.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Logger.Exceptions
+{
+    public class NoTokenException : Exception
+    {
+        public NoTokenException() : base() { }
+        public NoTokenException(string msg) : base(msg) { }
+    }
+
+    public class BadTokenException : Exception
+    {
+        public BadTokenException() : base() { }
+        public BadTokenException(string msg) : base(msg) { }
+    }
+
+    public class NoErrorCodeException : Exception
+    {
+        public NoErrorCodeException() : base() { }
+        public NoErrorCodeException(string msg) : base(msg) { }
+    }
+}
