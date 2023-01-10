6c35f36f37ee6de5cfd0f415935bd43d8bb35578
bissaye <bissaye.com@gmail.com>
2023-01-10 11:54:50 +0100

    log automatique des requetes entrantes

--- Middlewares/LoggerMiddleware.cs
+++ Middlewares/LoggerMiddleware.cs
@@ -1,4 +1,5 @@
 ﻿using Logger.Tools.Interfaces;
+using LoggerService = Logger.Services.Implementations.Logger;
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Http.Features;
@@ -8,17 +9,20 @@ using System.Linq;
 using System.Net.Http;
 using System.Text;
 using System.Threading.Tasks;
+using Logger.Tools.Implementations;
 
 namespace Logger.Middlewares
 {
     public class LoggerMiddleware
     {
         private readonly RequestDelegate _next;
+        private readonly LoggerService _logger;
         public LoggerMiddleware(RequestDelegate next)
         {
             _next = next;
+            _logger = new LoggerService();
         }
-
+        
         public async Task Invoke(HttpContext context)
         {
 
@@ -28,12 +32,33 @@ namespace Logger.Middlewares
             IDisplay._response = context.Response;
             IDisplay._clientIpAddress = context.Connection.RemoteIpAddress?.ToString();
             IDisplay._serverIpAddress = feature?.LocalIpAddress.ToString();
-
+            Stream originalBody = context.Response.Body;
+            try
+            {
+                var response = context.Response;
+                using (var memStream = new MemoryStream())
+                {
+                    context.Response.Body = memStream;
+                    await _next(context);
+                    memStream.Position = 0;
+                    string responseBody = new StreamReader(memStream).ReadToEnd();
+                    IDisplay._errorCode = IHttpRequestInfo.getOutGoingErrorCode(responseBody);
+                    _logger.logInformation("Incomming request");
+                }
+            }
+            finally
+            {
+                context.Response.Body = originalBody;
+            }
+            
+            
             await _next(context);
+
+            
         }
     }
 
-    public static class ALoggerMiddlewareExtensions
+    public static class LoggerMiddlewareExtensions
     {
         public static IApplicationBuilder UseALoggerMiddleware(this IApplicationBuilder app)
         {
